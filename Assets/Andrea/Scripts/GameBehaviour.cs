d37e6015ab459e499263ba524002c74927ec0c04
andreabefuen <al341488@uji.es>
2018-11-10 12:27:53 +0100

    Juego hecho a medias

---
+++ Assets/Andrea/Scripts/GameBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBehaviour : MonoBehaviour {
+
+    public InputField mainInput;
+
+    [SerializeField]
+    private Text[] spawnTexts;
+
+
+    private Text spawn1text;
+    private Text spawn2text;
+    private Text spawn3text;
+    private Text spawn4text;
+
+
+
+
+    private string inputStringUser;
+
+    private void Start()
+    {
+        spawn1text = spawnTexts[0];
+        spawn2text = spawnTexts[1];
+        spawn3text = spawnTexts[2];
+        spawn4text = spawnTexts[3];
+
+        //mainInput.onValueChanged.AddListener(delegate { OnTextChange(); });
+    }
+
+    void OnTextChange()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            inputStringUser = mainInput.text;
+            mainInput.text = "";
+
+            Debug.Log("Se ha escrito: " + inputStringUser);
+
+            for (int i = 0; i < spawnTexts.Length; i++)
+            {
+                if(spawnTexts[i].text == inputStringUser)
+                {
+                    Debug.Log("TA BIEN ESCRITO");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Andrea/Scripts/ReadWords.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class ReadWords : MonoBehaviour {
+
+    public TextAsset textFile;
+
+    
+
+    private string[] words  = new string[6];
+
+	// Use this for initialization
+	void Start () {
+        string text = textFile.text;
+        //Debug.Log(text);
+
+        
+
+
+        for (int i = 0; i < 6; i++)
+        {
+            string w = text.Split('\n')[i];
+
+            words[i] = w;
+        }
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            Debug.Log("Palabra " + i + " " + words[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+    void readTextFile()
+    {
+
+        
+
+    }
+}
