bbf680d7c460589663350493a59755542dc0de30
mariot11 <mariot3r@gmail.com>
2018-12-22 19:39:45 +0100

    menu de pausa inicial

---
+++ Assets/Mario/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (GameIsPaused) {
+                Resume();
+            }
+        }
+    }
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+}
